d2f09fdd0345d06db9fc9378eae38b635bdd6fad
Lior Katz <liorkatz21@gmail.com>
2026-05-19 14:20:26 +0300

    add tests for invalid escape sequences in char and string literals

---
+++ tests/ui/errors/invalid_symbol/escape_sequences/CharLiteralEscapedClosingQuote.java
@@ -0,0 +1,4 @@
+class Test {
+    // \' is treated as a an escaped single quote, therefore the character literal is unclosed
+    char a = '\';
+}
---
+++ tests/ui/errors/invalid_symbol/escape_sequences/CharLiteralLargeOctalEscapeIsTwoCharacters.java
@@ -0,0 +1,3 @@
+class Test {
+    char a = '\456'; // the escape is \45, and 6 is a second character in a char literal
+}
